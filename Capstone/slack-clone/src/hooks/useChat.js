b929044548094fbfb6b895dc461c5d20a8ad3a3a
BhoomiRaghav <bhoomiraghac01@gmail.com>
2026-05-02 19:25:06 +0530

    Remove duplicate Capstone folder from repo

--- Capstone/slack-clone/src/hooks/useChat.js
+++
@@ -1,9 +0,0 @@
-// src/hooks/useChat.js
-import { useContext } from 'react';
-import { ChatContext } from '../context/ChatContext';
-
-export function useChat() {
-  const ctx = useContext(ChatContext);
-  if (!ctx) throw new Error('useChat must be used within ChatProvider');
-  return ctx;
-}
